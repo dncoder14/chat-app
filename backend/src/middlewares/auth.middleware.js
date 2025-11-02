e8c78ebc5149d72fd59110303ee7927182f4ee00
dncoder14 <dhirajpandit5050@gmail.com>
2025-11-02 15:33:19 +0530

    feat: add authentication and authorization middleware

---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+import { prisma } from "../lib/prisma.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized - No Token Provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!decoded) {
+      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.userId },
+      select: {
+        id: true,
+        email: true,
+        fullName: true,
+        phone: true,
+        profilePic: true,
+        isOnline: true,
+        lastSeen: true,
+        contacts: true,
+        blockedUsers: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
